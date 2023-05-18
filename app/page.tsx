25d928275c73ca26ef15e9d89169b093d40bde5f
Max C <max@solers.io>
2023-05-18 16:23:48 +0200

    poc done

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,26 @@
 "use client";
 import Game from "@/app/game";
 import Head from "next/head";
+import { useEffect, useState } from "react";
+import { sampleSize } from "lodash";
+import { COUNTRIES } from "@/utils/countries";
+
+const SAMPLE_SIZE = 10;
 
 function Home() {
+  const [countries, setCountries] = useState<string[]>([]);
+
+  useEffect(() => {
+    const randomSample = sampleSize(COUNTRIES, SAMPLE_SIZE);
+    setCountries(randomSample);
+  }, []);
+
   return (
     <>
       <Head>
         <title>Interactive World Map</title>
       </Head>
-      <Game countries={["France", "Bangladesh", "Turkey"]} />
+      <Game countries={countries} />
     </>
   );
 }
