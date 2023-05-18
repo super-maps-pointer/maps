c6338b90780c5a19c72c334029476642836b4fae
Max C <max@solers.io>
2023-05-18 14:19:49 +0200

    add color

--- app/globe.tsx
+++ app/globe.tsx
@@ -1,35 +1,47 @@
-import React from "react";
+import useDeviceSize from "@/hooks/useDeviceSize";
+import React, { useState } from "react";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 
 const Globe = () => {
-  const handleCountryClick = (geography: any) => {
-    console.log("geography", geography);
-    const countryName = geography.properties.NAME;
+  const [selectedCountry, setSelectedCountry] = useState(null);
+  const [width, height] = useDeviceSize();
+
+  const handleCountryClick = (geography) => {
+    const countryName = geography.properties.name;
+    setSelectedCountry(countryName);
     console.log(`Clicked on: ${countryName}`);
   };
 
   return (
-    <ComposableMap
-      projectionConfig={{
-        scale: 250,
-      }}
-      width={800}
-      height={600}
-    >
-      <Geographies geography="/world-110m.json">
-        {({ geographies }) =>
-          geographies.map((geography) => (
-            <Geography
-              key={geography.rsmKey}
-              geography={geography}
-              stroke="#fff"
-              fill="#088"
-              onClick={() => handleCountryClick(geography)}
-            />
-          ))
-        }
-      </Geographies>
-    </ComposableMap>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <ComposableMap
+        projectionConfig={{
+          scale: 250,
+        }}
+        width={width}
+        height={height}
+      >
+        <Geographies geography="/world-110m.json">
+          {({ geographies }) =>
+            geographies.map((geography) => {
+              const countryName = geography.properties.name;
+              const isCountrySelected = countryName === selectedCountry;
+              const fill = isCountrySelected ? "#f00" : "#088";
+
+              return (
+                <Geography
+                  key={geography.rsmKey}
+                  geography={geography}
+                  stroke="#fff"
+                  fill={fill}
+                  onClick={() => handleCountryClick(geography)}
+                />
+              );
+            })
+          }
+        </Geographies>
+      </ComposableMap>
+    </div>
   );
 };
 
