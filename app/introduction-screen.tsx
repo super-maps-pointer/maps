c170ac8e33ef67f9cfd19a66c7edacbee8f0f466
Max Chrétien <chretien.max@gmail.com>
2023-05-19 14:22:50 +0200

    feat: logo (#3)
    * feat: add logo
    
    * add intro screen and logo
    
    * change font and background color

---
+++ app/introduction-screen.tsx
@@ -0,0 +1,31 @@
+import { FC } from "react";
+import Image from "next/image";
+import { IMAGES } from "@/utils/images";
+
+interface IntroductionScreenProps {
+  onPlay: () => void;
+}
+
+const IntroductionScreen: FC<IntroductionScreenProps> = ({ onPlay }) => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="mb-4">
+        <Image src={IMAGES.logo} alt="Image" width={400} height={400} />
+      </div>
+      <h1 className="text-4xl font-bold mb-4">
+        Welcome to Super Maps Pointer!
+      </h1>
+      <p className="text-xl mb-8">
+        Get ready to test your geography knowledge!
+      </p>
+      <button
+        className="px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-600"
+        onClick={onPlay}
+      >
+        Start Game
+      </button>
+    </div>
+  );
+};
+
+export default IntroductionScreen;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 "use client";
+import { useCallback, useState } from "react";
 import Game from "@/app/game";
 import Head from "next/head";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import IntroductionScreen from "@/app/introduction-screen";
 
 function Home() {
+  const [showIntroduction, setShowIntroduction] = useState(true);
+
+  const handlePlay = useCallback(() => {
+    setShowIntroduction(false);
+  }, []);
+
   return (
     <>
       <ToastContainer
@@ -14,8 +22,26 @@ function Home() {
       />
       <Head>
         <title>Interactive World Map</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
-      <Game />
+      {showIntroduction ? <IntroductionScreen onPlay={handlePlay} /> : <Game />}
     </>
   );
 }
