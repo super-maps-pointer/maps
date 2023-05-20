905cdbc4bd709740b9dc5d19fac5fbb22246bf2b
Max C <max@solers.io>
2023-05-20 13:35:52 +0200

    improve style of gauge

--- app/gauge.tsx
+++ app/gauge.tsx
@@ -12,16 +12,16 @@ const Gauge: FC<GaugeProps> = ({ score, winCondition }) => {
     <div className="absolute top-1/2 right-5 transform -translate-y-1/2 text-center">
       <div className="flex flex-col items-center">
         <div
-          className="h-64 w-4 bg-gray-300 mb-4"
+          className="h-80 w-4 bg-gray-300 mb-4 transform rotate-180 rounded-lg"
           style={{ marginBottom: "4px" }}
         >
           <div
-            className="h-full bg-blue-500"
+            className="h-full rounded-lg"
             style={{
               height: `${gaugeScore}%`,
-              transformOrigin: "top",
+              background: `linear-gradient(to top, #59c75c ${gaugeScore}%, #b6e3b9 100%)`,
             }}
-          ></div>
+          />
         </div>
         <p>
           {score} / {winCondition}
