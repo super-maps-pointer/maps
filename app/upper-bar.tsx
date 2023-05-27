cc757285b84f1e79ec46b5fd3737acb33af28c6f
Max C <max@solers.io>
2023-05-27 11:24:04 +0200

    better upper bar

--- app/upper-bar.tsx
+++ app/upper-bar.tsx
@@ -19,16 +19,16 @@ const UpperBar: FC<UpperBarProps> = ({
 }) => {
   return (
     <div className="absolute top-0 left-0 w-full bg-gray-300 px-4 py-2 border-b border-gray-400">
-      <h1 className="text-2xl font-bold text-center">
-        Guess the Country on the Map!
-      </h1>
-      <h2 className="text-2xl font-bold text-center">{countryToGuess}</h2>
       <div className="flex justify-between items-center">
-        <div>
+        <div className="flex flex-col">
           <h3 className="text-lg font-semibold">
             Tries: {tries} / {SAMPLE_SIZE}
           </h3>
           <h3 className="text-lg font-semibold">Projection: {geoProjection}</h3>
+        </div>
+        <div className="flex flex-col items-center">
+          <h1 className="text-2xl font-bold">Guess the Country on the Map!</h1>
+          <h2 className="text-2xl font-bold">{countryToGuess.toUpperCase()}</h2>
         </div>
         <div>{displayLevel(level)}</div>
       </div>
