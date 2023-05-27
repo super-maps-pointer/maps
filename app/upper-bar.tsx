0bcccaea9e98946d753ee288c7e5c0a5ff1268c1
Max C <max@solers.io>
2023-05-27 11:19:04 +0200

    center country to guess

--- app/upper-bar.tsx
+++ app/upper-bar.tsx
@@ -19,12 +19,10 @@ const UpperBar: FC<UpperBarProps> = ({
 }) => {
   return (
     <div className="absolute top-0 left-0 w-full bg-gray-300 px-4 py-2 border-b border-gray-400">
-      <div>
-        <h1 className="text-2xl font-bold text-center">
-          Guess the Country on the Map!
-        </h1>
-        <h2 className="text-2xl font-bold">{countryToGuess}</h2>
-      </div>
+      <h1 className="text-2xl font-bold text-center">
+        Guess the Country on the Map!
+      </h1>
+      <h2 className="text-2xl font-bold text-center">{countryToGuess}</h2>
       <div className="flex justify-between items-center">
         <div>
           <h3 className="text-lg font-semibold">
