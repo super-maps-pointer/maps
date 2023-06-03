78d277735231cee05b591119b8f17a0b325e0522
Max C <max@solers.io>
2023-06-03 10:15:45 +0200

    center text in upper part

--- app/upper-bar.tsx
+++ app/upper-bar.tsx
@@ -24,18 +24,28 @@ const UpperBar: FC<UpperBarProps> = ({
 }) => {
   return (
     <div className="absolute top-0 left-0 w-full bg-gray-300 px-4 py-2 border-b border-gray-400">
-      <div className="flex justify-between items-center">
-        <div className="flex flex-col">
-          <h3 className="text-lg font-semibold">
-            Tries: {tries} / {SAMPLE_SIZE}
-          </h3>
-          <h3 className="text-lg font-semibold">Projection: {geoProjection}</h3>
+      <div className="flex flex-col justify-between">
+        <div className="flex items-center">
+          <div className="flex-col flex-1">
+            <h3 className="text-left text-lg font-semibold">
+              Tries: {tries} / {SAMPLE_SIZE}
+            </h3>
+            <h3 className="text-left text-lg font-semibold">
+              Projection: {geoProjection}
+            </h3>
+          </div>
+          <div className="flex-1">
+            <h1 className="text-center text-2xl font-bold">
+              Guess the Country on the Map!
+            </h1>
+            <p className="text-center">
+              {countryToGuess !== null && renderCountryName(countryToGuess)}
+            </p>
+          </div>
+          <div className="flex-1">
+            <p className="text-right">{displayLevel(level)}</p>
+          </div>
         </div>
-        <div className="flex flex-col items-center">
-          <h1 className="text-2xl font-bold">Guess the Country on the Map!</h1>
-          {countryToGuess !== null && renderCountryName(countryToGuess)}
-        </div>
-        <div>{displayLevel(level)}</div>
       </div>
     </div>
   );
