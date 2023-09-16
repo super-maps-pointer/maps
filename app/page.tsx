e0d834413e908f7a63ad0f1b83a4d3c43e68984d
Max C <max@solers.io>
2023-09-16 17:13:56 +0200

    feat: add start sound

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,8 @@ import IntroductionScreen from "@/components/menu/introduction-screen";
 import NextLevelScreen from "@/components/menu/next-level-screen";
 import EndGameScreen from "@/components/menu/end-game-screen";
 import RetryLevelScreen from "@/components/menu/retry-level-screen";
-import Confetti from "@/components/game/confetti";
+import useSound from "use-sound";
+import { SOUNDS } from "@/utils/sounds";
 
 type ActiveState =
   | "introduction"
@@ -19,10 +20,12 @@ type ActiveState =
 function Home() {
   const [activeState, setActiveState] = useState<ActiveState>("introduction");
   const [level, setLevel] = useState<Level>(Level.Easy);
+  const [playStartSound] = useSound(SOUNDS.start);
 
   const handlePlay = useCallback(() => {
+    playStartSound();
     setActiveState("game");
-  }, []);
+  }, [playStartSound]);
 
   const handleNextLevel = useCallback(() => {
     setActiveState(level === Level.Extreme ? "endGame" : "nextLevel");
---
+++ utils/sounds.ts
@@ -0,0 +1,4 @@
+export const SOUNDS = {
+  start: "/sounds/start.mp3",
+  good: "/sounds/good.wav",
+};
