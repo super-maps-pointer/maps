e695b6e82c6c3a5e21d2a5f87d2d299921cbd304
Max Chrétien <chretien.max@gmail.com>
2023-11-01 15:33:52 +0100

    configure i18n (#34)

--- components/menu/end-game-screen.tsx
+++ components/menu/end-game-screen.tsx
@@ -1,10 +1,13 @@
 import { FC } from "react";
 import { Text } from "@chakra-ui/react";
+import { useTranslation } from "@/i18n/client";
 
 const EndGameScreen: FC = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="flex items-center justify-center h-screen flex-col">
-      <Text className="text-center">You finish the game! Amazing</Text>
+      <Text className="text-center">{t("end.congratulations")}</Text>
     </div>
   );
 };
--- components/menu/retry-level-screen.tsx
+++ components/menu/retry-level-screen.tsx
@@ -1,21 +1,24 @@
 import { FC } from "react";
 import { Button, Text } from "@chakra-ui/react";
+import { useTranslation } from "@/i18n/client";
 
 interface RetryLevelScreenProps {
   onPlay: () => void;
 }
 
 const RetryLevelScreen: FC<RetryLevelScreenProps> = ({ onPlay }) => {
+  const { t } = useTranslation();
+
   return (
     <div className="flex items-center justify-center h-screen flex-col">
-      <Text className="text-center">Game over! You failed.</Text>
+      <Text className="text-center">{t("retry.gameOver")}</Text>
       <Button
         rounded="full"
         colorScheme="secondary"
         variant="solid"
         onClick={onPlay}
       >
-        Play Again
+        {t("retry.playAgain")}
       </Button>
     </div>
   );
